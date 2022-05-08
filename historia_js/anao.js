1a56616aaea9b13f0c444aadca6c2c9b3a8a6261
Aline Marinho <alinemarinho.am13@gmail.com>
2022-05-08 13:42:30 -0300

    criação do projeto e conf js

---
+++ historia_js/anao.js
@@ -0,0 +1,24 @@
+function resposta() {
+    var resposta = prompt("1-esquerda <br> 2-direita ");
+    var tentativas = 1;
+
+    for (var i=0; i <tentativas; i++) {
+
+        if (resposta == "1") {
+            alert("siga seu caminho ")
+            window.location.href = "/anao1.html"
+        
+
+        } else if (resposta == "2") {
+            alert("bora encher o caneco ,é hoje que você só chega amanhã");
+
+        }
+           
+            
+    }
+}
+
+/*function validaçãoDeResposta() {
+    var reposta = [1,2] ;
+    var nrtentativas = 3 ; */
+
