442a3394b2f7d35ef0625bb5b3f16c18deb6c247
Aline Marinho <alinemarinho.am13@gmail.com>
2022-05-11 17:15:46 -0300

    alt personagem elfo

---
+++ historia_js/humano_fase2.js
@@ -0,0 +1,26 @@
+function resposta() {
+    var resposta = prompt("1-esquerda 2-direita ");
+    var tentativas = 1;
+
+    for (var i=0; i <tentativas; i++) {
+
+        if (resposta == "1") {
+            alert("siga seu caminho ");
+            location.href = "../historia_personagens_html/anao_fase2.html"
+            
+        
+
+        } else if (resposta == "2") {
+            alert("ah sinto muito mas você não ira continuar a jornada , voltar ao inicio ");
+           location.href ="../gameover.html"
+        }
+          else{
+              alert("Resposta inválida");
+          }
+           
+            
+    }
+}
+
+
+
